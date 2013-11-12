558f29f882d9d9a9cd4ecfe8bf137b0901f11bc1
Constantin Chuprik <constantinchuprik@gmail.com>
2013-11-12 22:59:02 +0700

    Доработки в X2 и геттеры в WMX2Response

--- api/WMX2Response.php
+++ api/WMX2Response.php
@@ -17,7 +17,7 @@ class WMX2Response extends WMApiResponse
     protected $_operationInternalId;
 
     /** @var int operation/tranid */
-    protected $_operationTransactionId;
+    protected $_operationExternalId;
 
     /** @var string operation/pursesrc */
     protected $_operationSenderPurse;
@@ -38,19 +38,19 @@ class WMX2Response extends WMApiResponse
     protected $_operationPeriod;
 
     /** @var int operation/wminvid */
-    protected $_operationWmInvoiceNumber;
+    protected $_operationInvoiceId;
 
     /** @var int operation/orderid */
-    protected $_operationOrderId;
+    protected $_operationExternalInvoiceId;
 
     /** @var string operation/desc */
     protected $_operationDescription;
 
     /** @var DateTime operation/datecrt */
-    protected $_operationCreateDatetime;
+    protected $_operationCreateDateTime;
 
     /** @var DateTime operation/dateupd */
-    protected $_operationUpdateDatetime;
+    protected $_operationUpdateDateTime;
 
     /**
      * @param string $response
@@ -65,18 +65,138 @@ class WMX2Response extends WMApiResponse
             $operation = $responseObject->operation;
             $this->_operationId = $operation['id'];
             $this->_operationInternalId = $operation['ts'];
-            $this->_operationTransactionId = $operation->tranid;
+            $this->_operationExternalId = $operation->tranid;
             $this->_operationSenderPurse = $operation->pursesrc;
             $this->_operationRecipientPurse = $operation->pursedest;
             $this->_operationAmount = $operation->amount;
             $this->_operationFee = $operation->comiss;
             $this->_operationType = $operation->opertype;
             $this->_operationPeriod = $operation->period;
-            $this->_operationWmInvoiceNumber = $operation->wminvid;
-            $this->_operationOrderId = $operation->orderid;
+            $this->_operationInvoiceId = $operation->wminvid;
+            $this->_operationExternalInvoiceId = $operation->orderid;
             $this->_operationDescription = $operation->desc;
-            $this->_operationCreateDatetime = self::_createDateTime((string)$operation->datecrt);
-            $this->_operationUpdateDatetime = self::_createDateTime((string)$operation->dateupd);
+            $this->_operationCreateDateTime = self::_createDateTime((string)$operation->datecrt);
+            $this->_operationUpdateDateTime = self::_createDateTime((string)$operation->dateupd);
         }
+    }
+
+    /**
+     * @return string
+     */
+    public function getReturnDescription()
+    {
+        return $this->_returnDescription;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationId()
+    {
+        return $this->_operationId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationInternalId()
+    {
+        return $this->_operationInternalId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationExternalId()
+    {
+        return $this->_operationExternalId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOperationSenderPurse()
+    {
+        return $this->_operationSenderPurse;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOperationRecipientPurse()
+    {
+        return $this->_operationRecipientPurse;
+    }
+
+    /**
+     * @return float
+     */
+    public function getOperationAmount()
+    {
+        return $this->_operationAmount;
+    }
+
+    /**
+     * @return float
+     */
+    public function getOperationFee()
+    {
+        return $this->_operationFee;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationType()
+    {
+        return $this->_operationType;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationPeriod()
+    {
+        return $this->_operationPeriod;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationInvoiceId()
+    {
+        return $this->_operationInvoiceId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOperationExternalInvoiceId()
+    {
+        return $this->_operationExternalInvoiceId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOperationDescription()
+    {
+        return $this->_operationDescription;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getOperationCreateDateTime()
+    {
+        return $this->_operationCreateDateTime;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getOperationUpdateDateTime()
+    {
+        return $this->_operationUpdateDateTime;
     }
 }
