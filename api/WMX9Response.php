428a1c65b791e0346b651dab86684ab8290a89f1
Constantin Chuprik <constantinchuprik@gmail.com>
2013-12-03 14:36:38 +0700

    Неверная обработка xml в X9Response

--- api/WMX9Response.php
+++ api/WMX9Response.php
@@ -22,7 +22,7 @@ class WMX9Response extends WMApiResponse
         $this->_requestNumber = (int)$responseObject->reqn;
         $this->_returnCode = (int)$responseObject->retval;
         $this->_returnDescription = (string)$responseObject->retdesc;
-        foreach ($responseObject->purses as $purse) {
+        foreach ($responseObject->purses->children() as $purse) {
             $this->_purses[] = new WMX9ResponsePurse($this->_purseToArray($purse));
         }
     }
