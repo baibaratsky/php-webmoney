f0518aad3f801ba4181909fefef4a229e80acdf7
baibaratsky <andrei@baibaratsky.com>
2014-03-25 10:53:02 +0400

    X14

---
+++ Api/X/X14/Request.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace baibaratsky\WebMoney\Api\X\X14;
+
+use baibaratsky\WebMoney\Api\X;
+use baibaratsky\WebMoney\Exception\ApiException;
+use baibaratsky\WebMoney\Request\RequestSigner;
+use baibaratsky\WebMoney\Request\RequestValidator;
+
+/**
+ * Class Request
+ *
+ * @link https://wiki.wmtransfer.com/projects/webmoney/wiki/Interface_X14
+ */
+class Request extends X\Request
+{
+    /** @var string wmid */
+    protected $signerWmid;
+
+    /** @var int trans\inwmtranid */
+    protected $transactionId;
+
+    /** @var float trans\amount */
+    protected $amount;
+
+    /** @var string trans\moneybackphone */
+    protected $phone;
+
+    /** @var string trans\capitallerpursesrc */
+    protected $capitallerPurse;
+
+    public function __construct($authType = self::AUTH_CLASSIC)
+    {
+        if ($authType === self::AUTH_CLASSIC) {
+            $this->url = 'https://w3s.webmoney.ru/asp/XMLTransMoneyback.asp';
+        } elseif ($authType === self::AUTH_LIGHT) {
+            $this->url = 'https://w3s.wmtransfer.com/asp/XMLTransMoneybackCert.asp';
+        } else {
+            throw new ApiException('This interface doesn\'t support the authentication type given.');
+        }
+
+        parent::__construct($authType);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getValidationRules()
+    {
+        return array(
+            RequestValidator::TYPE_REQUIRED => array('transactionId', 'amount'),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getData()
+    {
+        $xml = '<w3s.request>';
+        $xml .= self::xmlElement('reqn', $this->requestNumber);
+        $xml .= self::xmlElement('wmid', $this->signerWmid);
+        $xml .= self::xmlElement('sign', $this->signature);
+        $xml .= '<trans>';
+        $xml .= self::xmlElement('inwmtranid', $this->transactionId);
+        $xml .= self::xmlElement('amount', $this->amount);
+        $xml .= self::xmlElement('moneybackphone', $this->phone);
+        $xml .= self::xmlElement('capitallerpursesrc', $this->capitallerPurse);
+        $xml .= '</trans>';
+        $xml .= '</w3s.request>';
+
+        return $xml;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResponseClassName()
+    {
+        return Response::className();
+    }
+
+    /**
+     * @param RequestSigner $requestSigner
+     *
+     * @return void
+     */
+    public function sign(RequestSigner $requestSigner = null)
+    {
+        if ($this->authType === self::AUTH_CLASSIC) {
+            $this->signature = $requestSigner->sign($this->requestNumber . $this->transactionId . $this->amount);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getSignerWmid()
+    {
+        return $this->signerWmid;
+    }
+
+    /**
+     * @param string $signerWmid
+     */
+    public function setSignerWmid($signerWmid)
+    {
+        $this->signerWmid = $signerWmid;
+    }
+
+    /**
+     * @param int $transactionId
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = $transactionId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @param float $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $capitallerPurse
+     */
+    public function setCapitallerPurse($capitallerPurse)
+    {
+        $this->capitallerPurse = $capitallerPurse;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCapitallerPurse()
+    {
+        return $this->capitallerPurse;
+    }
+}
---
+++ Api/X/X14/Response.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace baibaratsky\WebMoney\Api\X\X14;
+
+use baibaratsky\WebMoney\Request\AbstractResponse;
+
+/**
+ * Class Response
+ *
+ * @link https://wiki.wmtransfer.com/projects/webmoney/wiki/Interface_X14
+ */
+class Response extends AbstractResponse
+{
+    /** @var int reqn */
+    protected $requestNumber;
+
+    /** @var int operation\@id */
+    protected $refundTransactionId;
+
+    /** @var int operation\@ts */
+    protected $refundTransactionInternalId;
+
+    /** @var int operation\inwmtranid */
+    protected $transactionId;
+
+    /** @var string operation/pursesrc */
+    protected $senderPurse;
+
+    /** @var string operation/pursedest */
+    protected $recipientPurse;
+
+    /** @var float operation\amount */
+    protected $amount;
+
+    /** @var float operation\comiss */
+    protected $fee;
+
+    /** @var float operation\desc */
+    protected $description;
+
+    /** @var \DateTime operation/datecrt */
+    protected $createDateTime;
+
+    /** @var \DateTime operation/dateupd */
+    protected $updateDateTime;
+
+    public function __construct($response)
+    {
+        $responseObject = new \SimpleXMLElement($response);
+        $this->requestNumber = (int)$responseObject->reqn;
+        $this->returnCode = (int)$responseObject->retval;
+        $this->returnDescription = (string)$responseObject->retdesc;
+        if (isset($responseObject->operation)) {
+            $operation = $responseObject->operation;
+            $this->refundTransactionId = (int)$operation['id'];
+            $this->refundTransactionInternalId = (int)$operation['ts'];
+            $this->transactionId = (int)$operation->inwmtranid;
+            $this->senderPurse = (string)$operation->pursesrc;
+            $this->recipientPurse = (string)$operation->pursdest;
+            $this->amount = (float)$operation->amount;
+            $this->fee = (float)$operation->comiss;
+            $this->description = (string)$operation->desc;
+            $this->createDateTime = self::createDateTime((string)$operation->datecrt);
+            $this->updateDateTime = self::createDateTime((string)$operation->dateupd);
+        }
+    }
+
+    /**
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreateDateTime()
+    {
+        return $this->createDateTime;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getFee()
+    {
+        return $this->fee;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRecipientPurse()
+    {
+        return $this->recipientPurse;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRefundTransactionId()
+    {
+        return $this->refundTransactionId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRefundTransactionInternalId()
+    {
+        return $this->refundTransactionInternalId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRequestNumber()
+    {
+        return $this->requestNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSenderPurse()
+    {
+        return $this->senderPurse;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdateDateTime()
+    {
+        return $this->updateDateTime;
+    }
+}
